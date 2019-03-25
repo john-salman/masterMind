d9b66d2665cfd30588b671e5cd80d276fba2cd73
John Salman <johnfish15@gmail.com>
2019-03-25 10:39:50 -0700

    Reworking board layout, old layouts in the copy files [copy.css, copy.js]

--- src/copy.js
+++ src/copy.js
@@ -13,37 +13,37 @@ const explosion = require('./images/explo.png');
 
 let uniqueSeed = 0;
 function nextUniqueKey() {
-  return uniqueSeed += 1;
+    return uniqueSeed += 1;
 }
 
 const NUM_ROWS = 6, NUM_COLUMNS = 5;
 
 class SecretCode extends Component {
-  render() {
-    return (
-        <td width="50px" height="50px" style={{backgroundColor: this.props.color}}>
+    render() {
+        return (
+            <td width="50px" height="50px" style={{backgroundColor: this.props.color}}>
 
-        </td>
-    )
-  }
+            </td>
+        )
+    }
 }
 
 class StatusRow extends Component {
-  render() {
-      let {
-          color,
-          colorName
-      } = this.props.statusCircle;
+    render() {
+        let {
+            color,
+            colorName
+        } = this.props.statusCircle;
 
-      return (<table className="status_circles">
-                <tbody>
-                    <tr>
-                        <td><img onClick={() => this.props.reset()} className="large_circle" src={red} alt="red circle" /></td>
-                        <td><img className="large_circle" src={color} alt={colorName} /></td>
-                    </tr>
-                </tbody>
-              </table>)
-  }
+        return (<table className="status_circles">
+            <tbody>
+            <tr>
+                <td><img onClick={() => this.props.reset()} className="large_circle" src={red} alt="red circle" /></td>
+                <td><img className="large_circle" src={color} alt={colorName} /></td>
+            </tr>
+            </tbody>
+        </table>)
+    }
 }
 
 
@@ -124,9 +124,9 @@ class MasterMindTable extends Component {
         let feedbackArray = this.props.feedbackArray ? this.props.feedbackArray : null;
 
         return (
-                    masterMindArray.map((current, index) => {
-                        return current[0] !== 0 ? <MasterMindRow idx={index} handleBoardClick={this.props.handleBoardClick} row={current} feedback={feedbackArray[index] ? feedbackArray[index] : null}/> : <div className="spacer"></div>;
-                    })
+            masterMindArray.map((current, index) => {
+                return current[0] !== 0 ? <MasterMindRow idx={index} handleBoardClick={this.props.handleBoardClick} row={current} feedback={feedbackArray[index] ? feedbackArray[index] : null}/> : <div className="spacer"></div>;
+            })
 
         )
 
@@ -134,17 +134,17 @@ class MasterMindTable extends Component {
 }
 
 class Palette extends Component {
-  render() {
-      return <table className="palette_circles"><tbody><tr>
-          {
-              this.props.paletteColors.map((paletteElement, idx) =>
-                  <td key={idx} >
-                      <img className="large_circle" onClick={() => this.props.handlePalClick(idx)} src={paletteElement.color} alt={paletteElement.colorName} />
-                  </td>
-              )
-          }
-      </tr></tbody></table>;
-  }
+    render() {
+        return <table className="palette_circles"><tbody><tr>
+            {
+                this.props.paletteColors.map((paletteElement, idx) =>
+                    <td key={idx} >
+                        <img className="large_circle" onClick={() => this.props.handlePalClick(idx)} src={paletteElement.color} alt={paletteElement.colorName} />
+                    </td>
+                )
+            }
+        </tr></tbody></table>;
+    }
 }
 
 class Difficulty extends Component {
@@ -155,30 +155,6 @@ class Difficulty extends Component {
                 <button onClick={() => this.props.raiseDifficulty}>Raise Difficulty?</button>
             </div>
         )
-    }
-}
-
-class Lost extends Component {
-    render() {
-        if (this.props.lost) {
-            return (
-                <div className="explosion">
-                    <img src={explosion} alt="An explosion"/>
-                    <div className="explosion-text-box">
-                        <p>
-                            YOU LOST D:
-                            <button onClick={this.props.reset}>RESET?
-                            </button>
-                        </p>
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-             <div></div>
-            )
-        }
-
     }
 }
 
@@ -199,136 +175,136 @@ class App extends Component {
     };
 
     constructor(props) {
-    super (props);
+        super (props);
 
-    let currentRow = 6; // 6
-    let firstRow = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
+        let currentRow = 6; // 6
+        let firstRow = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
 
-    let code = this.createCode();
-    this.state = {
-        mastermindArray: [[0], [0], [0], [0], [0], [0], firstRow],
-        feedbackArray: [],
-        statusCircle: {color: emptyCircle, colorName: 'Empty circle'},
-        currentRow: currentRow,
-        code: code,
-        fill: 0
-    };
+        let code = this.createCode();
+        this.state = {
+            mastermindArray: [[0], [0], [0], [0], [0], [0], firstRow],
+            feedbackArray: [],
+            statusCircle: {color: emptyCircle, colorName: 'Empty circle'},
+            currentRow: currentRow,
+            code: code,
+            fill: 0
+        };
 
-    console.log("Here's the code you cheater: ", code);
+        console.log("Here's the code you cheater: ", code);
 
 
-    this.reset = this.reset.bind(this);
-    this.lowerDifficulty = this.lowerDifficulty.bind(this);
-    this.raiseDifficulty = this.raiseDifficulty.bind(this);
+        this.reset = this.reset.bind(this);
+        this.lowerDifficulty = this.lowerDifficulty.bind(this);
+        this.raiseDifficulty = this.raiseDifficulty.bind(this);
 
-    this.handlePalClick = this.handlePalClick.bind(this);
-    this.handleBoardClick = this.handleBoardClick.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+        this.handlePalClick = this.handlePalClick.bind(this);
+        this.handleBoardClick = this.handleBoardClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
 
-  }
+    }
 
-  reset() {
+    reset() {
 
-      let firstRow = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
+        let firstRow = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
 
-      let currentRow = 6;
-      let numCols = 4;
+        let currentRow = 6;
+        let numCols = 4;
 
-      let code = this.createCode();
+        let code = this.createCode();
 
-      this.setState({
-          mastermindArray: [[0], [0], [0], [0], [0], [0], firstRow],
-          feedbackArray: [],
-          statusCircle: {color: emptyCircle, colorName: 'Empty circle'},
-          currentRow: currentRow,
-          code: code,
-          fill: 0,
-          correct: false,
-          lost: false,
-          numCols: numCols
-      });
-    console.log("=============================RESET=============================");
-      console.log("Here's the code you cheater: ", code);
-  }
+        this.setState({
+            mastermindArray: [[0], [0], [0], [0], [0], [0], firstRow],
+            feedbackArray: [],
+            statusCircle: {color: emptyCircle, colorName: 'Empty circle'},
+            currentRow: currentRow,
+            code: code,
+            fill: 0,
+            correct: false,
+            lost: false,
+            numCols: numCols
+        });
+        console.log("=============================RESET=============================");
+        console.log("Here's the code you cheater: ", code);
+    }
 
-  lowerDifficulty(){
+    lowerDifficulty(){
 
-  }
+    }
 
-  raiseDifficulty() {
+    raiseDifficulty() {
 
-  }
+    }
 
-  handleClick() {
-    this.setState (
-        {statusCircle: this.nonFilledCircle}
-    );
-  }
-
-  handlePalClick(palIdx) {
+    handleClick() {
         this.setState (
-        {statusCircle: this.paletteColors[palIdx]}
-    )
-  }
-
-  handleBoardClick(rowIdx, colIdx) {
-    if (this.state.correct || this.state.lost) {
-      console.log("correct/lost triggered");
-      return;
+            {statusCircle: this.nonFilledCircle}
+        );
     }
-    console.log('current row = ', this.state.currentRow);
-    if (this.state.mastermindArray[rowIdx][colIdx]['colorName'] !== 'Empty circle') {
-        console.log("Color filled triggered, the index: ", colIdx, " the row: ", this.state.currentRow," what is in this index: ", this.state.mastermindArray[this.state.currentRow][colIdx]['colorName'],);
-        return;
-      }
 
-    let newRow = JSON.parse(JSON.stringify(this.state.mastermindArray[rowIdx].slice()));
-    newRow[colIdx] = JSON.parse(JSON.stringify(this.state.statusCircle));
+    handlePalClick(palIdx) {
+        this.setState (
+            {statusCircle: this.paletteColors[palIdx]}
+        )
+    }
 
-    let newArray = JSON.parse(JSON.stringify(this.state.mastermindArray.slice()));
-    newArray[rowIdx] = newRow;
-
-    /*
-    * We should probably make a new fill.
-    * Also, we dont need to check for a full fill yet as
-    * the color check above will prevent overflow.
-    * */
-    let newFill = this.state.fill + 1;
-
-    console.log("The fill is now: ", newFill);
-    let newCRow = null;
-    if (newFill === 4) {
-        console.log("Now entering fill protected code with fill: ", this.state.fill);
-
-        if (this.isCodeCorrect(newRow)) { // the row matches the secret code
-            this.setState(
-                {correct: true}
-            );
-        } else if (rowIdx === 0) { // we have filled the last row
-            this.setState(
-                {lost: true}
-            )
-        } else { // the row didn't match and user still has tries
-            newFill = 0;
-            newCRow = this.state.currentRow - 1;
-            newArray[newCRow] = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
-            //this.createNewRow();
+    handleBoardClick(rowIdx, colIdx) {
+        if (this.state.correct || this.state.lost) {
+            console.log("correct/lost triggered");
+            return;
         }
+        console.log('current row = ', this.state.currentRow);
+        if (this.state.mastermindArray[rowIdx][colIdx]['colorName'] !== 'Empty circle') {
+            console.log("Color filled triggered, the index: ", colIdx, " the row: ", this.state.currentRow," what is in this index: ", this.state.mastermindArray[this.state.currentRow][colIdx]['colorName'],);
+            return;
+        }
+
+        let newRow = JSON.parse(JSON.stringify(this.state.mastermindArray[rowIdx].slice()));
+        newRow[colIdx] = JSON.parse(JSON.stringify(this.state.statusCircle));
+
+        let newArray = JSON.parse(JSON.stringify(this.state.mastermindArray.slice()));
+        newArray[rowIdx] = newRow;
+
+        /*
+        * We should probably make a new fill.
+        * Also, we dont need to check for a full fill yet as
+        * the color check above will prevent overflow.
+        * */
+        let newFill = this.state.fill + 1;
+
+        console.log("The fill is now: ", newFill);
+        let newCRow = null;
+        if (newFill === 4) {
+            console.log("Now entering fill protected code with fill: ", this.state.fill);
+
+            if (this.isCodeCorrect(newRow)) { // the row matches the secret code
+                this.setState(
+                    {correct: true}
+                );
+            } else if (rowIdx === 0) { // we have filled the last row
+                this.setState(
+                    {lost: true}
+                )
+            } else { // the row didn't match and user still has tries
+                newFill = 0;
+                newCRow = this.state.currentRow - 1;
+                newArray[newCRow] = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
+                //this.createNewRow();
+            }
+        }
+        console.log("The new current row: ", newCRow);
+        this.setState(
+            {mastermindArray: newArray,
+                fill: newFill,
+                currentRow: newCRow ? newCRow: this.state.currentRow,
+                //statusCircle: {color: emptyCircle, colorName: 'Empty circle'},
+            }
+        )
+
+
+
     }
-    console.log("The new current row: ", newCRow);
-    this.setState(
-          {mastermindArray: newArray,
-              fill: newFill,
-              currentRow: newCRow ? newCRow: this.state.currentRow,
-              //statusCircle: {color: emptyCircle, colorName: 'Empty circle'},
-          }
-      )
-
-
-
-  }
-  createNewRow() {
-      let newCRow = this.state.currentRow + 1;
+    createNewRow() {
+        let newCRow = this.state.currentRow + 1;
         let newRow = [this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle, this.nonFilledCircle];
         let newArray = JSON.parse(JSON.stringify(this.state.mastermindArray.slice()));
         newArray[newCRow] = newRow;
@@ -339,21 +315,21 @@ class App extends Component {
                 currentRow: newCRow
             }
         );
-  }
+    }
 
     getRandomIdx(low, high) {
         return Math.floor(Math.random() * (high - low + 1) + low);
     }
 
-  createCode() {
+    createCode() {
         let code = [];
         for (let i = 0; i < 4; i++) {
             code[i] = this.paletteColors[this.getRandomIdx(0, 5)]
         }
         return code;
-  }
+    }
 
-  isCodeCorrect(row) {
+    isCodeCorrect(row) {
         // create feedback circles
         let feedbackRow = [];
         console.log("The row as it be: ", row);
@@ -367,21 +343,21 @@ class App extends Component {
                 feedbackRow[frevIndex--] = {color: red, colorName: 'red'};
                 codeCopy[index] = null;
                 console.log("Index ", index, " matches code exactly");
-            } 
+            }
         });
-      console.log("The code copy after first loop: ", JSON.parse(JSON.stringify(codeCopy)));
+        console.log("The code copy after first loop: ", JSON.parse(JSON.stringify(codeCopy)));
 
-      row.forEach((current, index) => {
-          console.log("current: ", current, " index: ", index);
+        row.forEach((current, index) => {
+            console.log("current: ", current, " index: ", index);
             if (codeCopy.find(obj => obj !== null && obj['colorName'] === current['colorName']) !== undefined){ // add a skeleton circle
                 feedbackRow[frevIndex--] = this.nonFilledCircle;
                 codeCopy[index] = null;
                 console.log("Index ", index, " has a color of the code");
             }
         });
-      console.log("The code copy after second loop: ", JSON.parse(JSON.stringify(codeCopy)));
+        console.log("The code copy after second loop: ", JSON.parse(JSON.stringify(codeCopy)));
 
-      for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < 4; i++) {
             feedbackRow[i] = feedbackRow[i] ? feedbackRow[i] : null;
         }
 
@@ -393,11 +369,11 @@ class App extends Component {
 
         return numCorrect === 4;
 
-  }
+    }
 
 
 
-  render() {
+    render() {
         if (this.state.correct) {
             return (
                 <div className="victory-screen">
@@ -406,12 +382,21 @@ class App extends Component {
                         <button onClick={this.reset}>RESET?
                         </button>
                     </p>
-            </div>
+                </div>
+            )
+        } else if (this.state.lost) {
+            return (
+                <div className="defeat-screen">
+                    <p>
+                        YOU LOST D:
+                        <button onClick={this.reset}>RESET?
+                        </button>
+                    </p>
+                </div>
             )
         } else {
             return (
                 <div className="outter">
-                    <Lost lost={this.state.lost} reset={this.reset}/>
                     <div className="inner">
                         <div className="Mastermind">
                             <StatusRow reset={this.reset} statusCircle={this.state.statusCircle}/>
@@ -438,7 +423,7 @@ class App extends Component {
             )
         }
 
-  }
+    }
 }
 
 export default App;
